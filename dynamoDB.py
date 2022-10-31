24bac37c33a4fd1874c2f589ec0b067cfd6111c4
ariel <ariel1434@gmail.com>
2022-10-31 12:55:44 +0200

    adding notes to dynamoDB.py

--- dynamoDB.py
+++ dynamoDB.py
@@ -2,6 +2,33 @@ import boto3
 
 dynamoClient = boto3.client('dynamodb')
 
+# usersTable = dynamoClient.create_table(
+#     TableName = 'users',
+#     KeySchema = [
+#         {
+#             'AttributeName': 'name',
+#             'KeyType': 'HASH'
+#         },
+#         {
+#             'AttributeName': 'ID',
+#             'KeyType': 'RANGE'
+#         },
+#     ],
+#     AttributeDefinitions = [
+#         {
+#             'AttributeName': 'name',
+#             'AttributeType': 'S'
+#         },
+#         {
+#             'AttributeName': 'ID',
+#             'AttributeType': 'S'
+#         },
+#     ],
+#     ProvisionedThroughput = {
+#         'ReadCapacityUnits': 10,
+#         'WriteCapacityUnits': 10
+#     }
+# )
 
 
 key = {
