731e44c7b5b01d61070db52ebc715ff4e8a3c1cf
zoharn007 <zphar.nagar.msn@gmail.com>
2023-01-25 22:22:31 +0200

    repeater.yaml

--- services/repeater/repeater.py
+++ services/repeater/repeater.py
@@ -5,13 +5,14 @@ import json
 import sys
 sys.path.insert(0, '/app/secrets')
 import key
-
+sys.path.insert(0, '/app/secrets2')
+import key2
 
 def GetDataFromFootballAPI():
 
     baseUrl = "https://livescore-api.com/api-client/"
     apiKey = key.apiKey
-    apiSecret = key.apiSecret
+    apiSecret = key2.apiSecret
     keyParam = { "key" : apiKey, "secret" : apiSecret }
 
     response = requests.get(baseUrl + "scores/live.json", params=keyParam) # READ
