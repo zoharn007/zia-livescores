290c02346147d55a5f22b46cc8d77a950d95dffe
ariel <ariel1434@gmail.com>
2023-01-30 14:13:01 +0200

    Update backend.py and repeater.py code for the latest build

--- services/backend/backend.py
+++ services/backend/backend.py
@@ -11,15 +11,17 @@ from botocore.exceptions import ClientError
 import random
 from decimal import Decimal
 import os
+from flask import jsonify
+from flask_cors import CORS
 
 dynamoResource = boto3.resource('dynamodb', region_name='eu-west-1')
-table = dynamoResource.Table('LiveScore')
+table = dynamoResource.Table('Daily')
 dynamoClient = boto3.client('dynamodb', region_name='eu-west-1')
 
-snsClient = boto3.client('sns', region_name='eu-west-1')
+snsClient = boto3.client('sns', region_name='eu-central-1')
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 def renderMainPage():
@@ -54,7 +56,7 @@ def sendSMSInternal(phone: str, message: str):
     print(message)
 
 
-@app.route('/enterCode')
+@app.route('/enterCode', methods=['GET', 'POST'])
 def enterCode():
     messageBody = json.loads(request.data)
 
@@ -63,11 +65,11 @@ def enterCode():
         phoneNumber = messageBody['Phone']
 
     except:
-        return "No code provided."
+        return jsonify("No code provided.")
 
     returnValue = checkIfCodePhoneComboExists(phoneNumber, code)
 
-    if (returnValue > 0):
+    if returnValue > 0:
 
         key = {
             'Phone': {'S': str(phoneNumber)},
@@ -78,17 +80,19 @@ def enterCode():
 
         registerUser(phoneNumber)
 
-        return "Registered successfully!"
+        return jsonify("Registered successfully!")
 
-    elif (returnValue == 0):
-        return "Incorrect code!"
+    elif returnValue == 0:
+        return jsonify("Incorrect code!")
 
-    elif (returnValue == -1):
-        return "Code expired!"
+    elif returnValue == -1:
+        return jsonify("Code expired!")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    print(request.data)
+
     messageBody = json.loads(request.data)
 
     try:
@@ -97,12 +101,12 @@ def register():
     except:
         return "No phone number provided."
 
-    if (checkIfPhoneNumberExists(phoneNumber)):
+    if checkIfPhoneNumberExists(phoneNumber):
         return "Account already exists, you cannot register."
 
     else:
         sendCodeToUser(phoneNumber)
-        return "Ok"
+        return jsonify("Ok")
 
 
 def sendCodeToUser(phoneNumber: str):
@@ -158,6 +162,30 @@ def checkIfPhoneNumberExists(phoneNumber: str):
     return len(itemsDict)
 
 
+def checkIfMatchExists(matchID: str):
+    dailyTable = dynamoResource.Table('Daily')
+
+    response = dailyTable.scan(
+        FilterExpression=Attr('ID').eq(matchID)
+    )
+
+    itemsDict = response['Items']
+
+    return len(itemsDict)
+
+
+def getUserObjectFromPhoneNumber(phoneNumber: str):
+    usersTable = dynamoResource.Table('users')
+
+    response = usersTable.scan(
+        FilterExpression=Attr('Phone').eq(phoneNumber)
+    )
+
+    itemsDict = response['Items']
+
+    return itemsDict[0]
+
+
 def checkIfCodePhoneComboExists(phone: str, code: str):
     usersTable = dynamoResource.Table('codes')
 
@@ -169,7 +197,14 @@ def checkIfCodePhoneComboExists(phone: str, code: str):
 
     end = time.time()
 
-    if (validate_code(itemsDict[0]['Seconds'], end) == 0):
+    if validate_code(itemsDict[0]['Seconds'], Decimal(end)) == 0:
+        key = {
+            'Phone': {'S': str(phone)},
+            'Code': {'S': str(code)}
+        }
+
+        dynamoClient.delete_item(TableName='codes', Key=key)
+
         sendCodeToUserAfterLastCodeExpired(itemsDict[0]['Phone'])
         return -1
 
@@ -177,7 +212,7 @@ def checkIfCodePhoneComboExists(phone: str, code: str):
 
 
 def validate_code(start, end):
-    if (end - start > 30):
+    if end - start > 30:
         return 0
     else:
         return 1
@@ -189,15 +224,17 @@ def sendMessageToPhoneNumber(phoneNumber: str, message: str):
 
 def get_result(match):
     response = table.query(KeyConditionExpression=Key('ID').eq(match))
-    return str(response['Items'][0]['Home']) + " " + str(response['Items'][0]['Score']) + " " + str(response['Items'][0]['Away'])
+    return str(response['Items'][0]['Home']) + " " + str(response['Items'][0]['Score']) + " " + str(
+        response['Items'][0]['Away'])
 
 
 def get_time(match):
     response = table.query(KeyConditionExpression=Key('ID').eq(match))
     return response['Items'][0]['Time']
 
+
 def checkIfTeamExists(matchID: str):
-    teamsTable = dynamoResource.Table('LiveScore')
+    teamsTable = dynamoResource.Table('Daily')
 
     response = teamsTable.scan(
         FilterExpression=Attr('ID').eq(matchID)
@@ -207,7 +244,8 @@ def checkIfTeamExists(matchID: str):
 
     return len(itemsDict)
 
-@app.route ('/score')
+
+@app.route('/score')
 def get_match_score():
     messageBody = json.loads(request.data)
     try:
@@ -221,7 +259,7 @@ def get_match_score():
         return "No such team, please provide a different team name."
 
 
-@app.route ('/time')
+@app.route('/time')
 def get_match_time():
     messageBody = json.loads(request.data)
     try:
@@ -234,9 +272,9 @@ def get_match_time():
     else:
         return "No such team, please provide a different team name."
 
-@app.route ('/repeater')
-def get_from_repeater():
 
+@app.route('/repeater')
+def get_from_repeater():
     try:
         messageBody = json.loads(request.data)
 
@@ -251,7 +289,7 @@ def get_from_repeater():
         date = messageBody['added']
 
         parsedDayTime = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S').date()
-        
+
         day = parsedDayTime.day
         month = parsedDayTime.month
         year = parsedDayTime.year
@@ -282,12 +320,27 @@ def get_from_repeater():
     print("import to DB completed successfully")
 
     return "import to DB completed successfully"
-    #if checkIfTeamExists(id):
+    # if checkIfTeamExists(id):
     #    return get_time(id)
-    #else:
+    # else:
     #    return "No such team, please provide a different team name."
 
-@app.route('/sign_in')
+
+@app.route('/disconnect', methods=['GET', 'POST'])
+def disconnect():
+    messageBody = json.loads(request.data)
+    try:
+        phoneNumber = messageBody['Phone']
+    except:
+        return jsonify("Please provide a phone number")
+
+    if checkIfPhoneNumberExists(phoneNumber):
+        setLoggedInFlagInDatabase(phoneNumber, False)
+
+    return jsonify("OK")
+
+
+@app.route('/sign_in', methods=['GET', 'POST'])
 def sign_in():
     messageBody = json.loads(request.data)
     try:
@@ -295,30 +348,32 @@ def sign_in():
 
     except:
 
-        return "No phone number provided."
-    if (checkIfPhoneNumberExists(phoneNumber)):
+        return jsonify("No phone number provided.")
+    if checkIfPhoneNumberExists(phoneNumber):
 
         setLoggedInFlagInDatabase(phoneNumber, True)
 
-        return "successfully logged in"
+        return jsonify("successfully logged in")
     else:
-        return "phone number not found please register"
+        return jsonify("phone number not found please register")
+
 
 def setLoggedInFlagInDatabase(phoneNumber, state):
-
-    if(checkIfPhoneNumberExists(phoneNumber)):
+    if checkIfPhoneNumberExists(phoneNumber):
+        userObject = getUserObjectFromPhoneNumber(phoneNumber)
 
         user = {
             'Phone': {'S': phoneNumber},
-            'LoggedIn': {'S': state}
+            'LoggedIn': {'BOOL': state},
+            'ID': {'S': userObject['ID']}
         }
 
         dynamoClient.put_item(Item=user, TableName='users')
 
     return
 
-def checkLoggedInFlag(phoneNumber):
 
+def checkLoggedInFlag(phoneNumber):
     usersTable = dynamoResource.Table('users')
 
     response = usersTable.scan(
@@ -327,21 +382,46 @@ def checkLoggedInFlag(phoneNumber):
 
     itemsDict = response['Items']
 
-    print(itemsDict[0]['LoggedIn'])
-
     return itemsDict[0]['LoggedIn']
 
-def subscribePhoneNumberToMatchID(phoneNumber, matchID):
 
-    if(not checkLoggedInFlag(phoneNumber)):
+@app.route("/subscribe", methods=['GET', 'POST'])
+def subscribe():
+    messageBody = json.loads(request.data)
+
+    try:
+        phoneNumber = messageBody['Phone']
+        matchID = messageBody['MatchID']
+    except:
+        return jsonify("No phone number or matchID provided.")
+
+    if not checkIfPhoneNumberExists(phoneNumber):
+        return jsonify("Not registered. Please register.")
+
+    if not checkLoggedInFlag(phoneNumber):
+        return jsonify("Not logged in.")
+
+    if checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID):
+        return jsonify("Already subscribed to match.")
+
+    if not checkIfMatchExists(matchID):
+        return jsonify("Nice trick, match ID doesn't exist. !" + str(matchID) + "!")
+
+    subscribePhoneNumberToMatchID(phoneNumber, matchID)
+
+    return jsonify("Ok")
+
+
+def subscribePhoneNumberToMatchID(phoneNumber, matchID):
+    if not checkLoggedInFlag(phoneNumber):
         return "Not logged in"
 
-    if(checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID)):
+    if checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID):
         return "Duplicate subscription detected"
 
     user = {
-        'ID': {'S': uuid.uuid4()},
-        'Phone': {'S': phoneNumber}, 
+        'ID': {'S': str(uuid.uuid4())},
+        'Phone': {'S': phoneNumber},
         'MatchID': {'S': matchID}
     }
 
@@ -349,8 +429,35 @@ def subscribePhoneNumberToMatchID(phoneNumber, matchID):
 
     return
 
-def checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID):
 
+@app.route("/unsubscribe", methods=['GET', 'POST'])
+def unsubscribe():
+    messageBody = json.loads(request.data)
+
+    try:
+        phoneNumber = messageBody['Phone']
+        matchID = messageBody['MatchID']
+    except:
+        return jsonify("No phone number or matchID provided.")
+
+    if not checkIfPhoneNumberExists(phoneNumber):
+        return jsonify("Not registered. Please register.")
+
+    if not checkLoggedInFlag(phoneNumber):
+        return jsonify("Not logged in.")
+
+    if not checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID):
+        return jsonify("Not subscribed to match.")
+
+    if not checkIfMatchExists(matchID):
+        return jsonify("Nice trick, match ID doesn't exist. !" + str(matchID) + "!")
+
+    deleteSubscription(phoneNumber, matchID)
+
+    return jsonify("Ok")
+
+
+def checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matchID):
     subscriptionsTable = dynamoResource.Table('Subscriptions')
 
     response = subscriptionsTable.scan(
@@ -361,62 +468,80 @@ def checkIfPhoneNumberAndMatchIDComboExistInSubscriptionsTable(phoneNumber, matc
 
     return len(itemsDict)
 
-@app.route('/hey')
-def checkDatabaseForScoreChangesInMatchesAndSendSMSWithResult():
 
+@app.route('/checkMatches')
+def checkDatabaseForScoreChangesInMatchesAndSendSMSWithResult():
     dailyMatches = getListOfDailyMatches()
 
     try:
         for match in dailyMatches:
 
-            if(match['Time'] == "FT"):
+            if match['Time'] == "FT":
 
-                subscriptionList = getSubscriptionListByMatchID(match['id'])
+                subscriptionList = getSubscriptionListByMatchID(match['ID'])
 
                 for subscriber in subscriptionList:
-
-                    sendSMSInternal(str(subscriber['Phone']), str(match['score']))
-                    deleteSubscription(subscriber['Phone'], match['id'])
+                    sendSMSInternal(str(subscriber['Phone']),
+                                    str(match['Home']) + " - " + str(match['Away'] + "\n" + str(match['Score'])))
+                    deleteSubscription(subscriber['Phone'], match['ID'])
 
     except:
 
-        return "not ok"
+        return jsonify(msg="not ok")
 
-    return "ok"
+    return jsonify("ok")
+
 
 def getScoreByMatchID(matchID):
-    
     if checkIfTeamExists(matchID):
         return get_result(matchID)
     else:
         return "No such team, please provide a different team name."
 
-def getListOfDailyMatches():
 
+@app.route('/getdailymatches', methods=['GET', 'POST'])
+def getDailyMatches():
     currentTime = time.localtime(time.time())
 
-    teamsTable = dynamoResource.Table('LiveScore')
+    teamsTable = dynamoResource.Table('Daily')
 
     response = teamsTable.scan(
-        FilterExpression=Attr('Day').eq(currentTime.tm_mday) and Attr('Month').eq(currentTime.tm_mon) and Attr('Year').eq(currentTime.tm_year)
+        FilterExpression=Attr('Day').eq(currentTime.tm_mday) and Attr('Month').eq(currentTime.tm_mon) and Attr(
+            'Year').eq(currentTime.tm_year)
+    )
+
+    itemsDict = response['Items']
+
+    return jsonify(itemsDict)
+
+
+def getListOfDailyMatches():
+    currentTime = time.localtime(time.time())
+
+    teamsTable = dynamoResource.Table('Daily')
+
+    response = teamsTable.scan(
+        FilterExpression=Attr('Day').eq(currentTime.tm_mday) and Attr('Month').eq(currentTime.tm_mon) and Attr(
+            'Year').eq(currentTime.tm_year)
     )
 
     itemsDict = response['Items']
 
     return itemsDict
 
-def getListOfHistoryMatches(startDate, endDate): #inclusive
+def getListOfHistoryMatches(startDate, endDate):  # inclusive
 
     startTime = time.localtime(startDate)
     endTime = time.localtime(endDate)
 
-    teamsTable = dynamoResource.Table('LiveScore')
+    teamsTable = dynamoResource.Table('Daily')
 
     response = teamsTable.scan(
         FilterExpression=
-            (Attr('Day').gte(startTime.tm_mday) and Attr('Month').gte(startTime.tm_mon) and Attr('Year').gte(startTime.tm_year))
-            and
-            (Attr('Day').lte(endTime.tm_mday) and Attr('Month').lte(endTime.tm_mon) and Attr('Year').lte(endTime.tm_year))
+        (Attr('Day').gte(startTime.tm_mday) and Attr('Month').gte(startTime.tm_mon) and Attr('Year').gte(
+            startTime.tm_year))
+        and
+        (Attr('Day').lte(endTime.tm_mday) and Attr('Month').lte(endTime.tm_mon) and Attr('Year').lte(endTime.tm_year))
 
     )
 
@@ -424,19 +549,32 @@ def getListOfHistoryMatches(startDate, endDate): #inclusive
 
     return itemsDict
 
+
+def GetSubscriptionIDByMatchIDAndPhoneNumberCombo(phoneNumber, matchID):
+    subscriptionsTable = dynamoResource.Table('Subscriptions')
+
+    response = subscriptionsTable.scan(
+        FilterExpression=Attr('Phone').eq(phoneNumber) and Attr('MatchID').eq(matchID)
+    )
+
+    itemsDict = response['Items']
+
+    return itemsDict[0]['ID']
+
+
 def deleteSubscription(phoneNumber, matchID):
+    ID = GetSubscriptionIDByMatchIDAndPhoneNumberCombo(phoneNumber, matchID)
 
     key = {
-        'Phone': {'S': phoneNumber},
-        'MatchID': {'S': matchID}
+        'ID': {'S': ID}
     }
 
-    dynamoClient.delete_item(TableName = 'Subscriptions', Key = key)
+    dynamoClient.delete_item(TableName='Subscriptions', Key=key)
 
     return
 
-def getSubscriptionListByMatchID(matchID):
 
+def getSubscriptionListByMatchID(matchID):
     teamsTable = dynamoResource.Table('Subscriptions')
 
     response = teamsTable.scan(
@@ -446,11 +584,7 @@ def getSubscriptionListByMatchID(matchID):
     return response['Items']
 
 
-
-
-
-
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    app.run()
 
 if __name__ == "__main__":
--- services/repeater/repeater.py
+++ services/repeater/repeater.py
@@ -9,13 +9,12 @@ sys.path.insert(0, '/app/secrets2')
 import key2
 
 def GetDataFromFootballAPI():
-
     baseUrl = "https://livescore-api.com/api-client/"
     apiKey = key.apiKey
     apiSecret = key2.apiSecret
     keyParam = { "key" : apiKey, "secret" : apiSecret }
 
-    response = requests.get(baseUrl + "scores/live.json", params=keyParam) # READ
+    response = requests.get(baseUrl + "scores/live.json", params=keyParam)  # READ
 
     x = json.loads(response.text)
 
@@ -23,7 +22,7 @@ def GetDataFromFootballAPI():
 
 
 def UpdateDatabaseWithMatchInformation(match):
-    baseUrlBackend = "http://backend-app-dev.dev:5000/"
+    baseUrlBackend = "http://127.0.0.1:5000/"
 
     try:
         parameters = \
@@ -31,51 +30,78 @@ def UpdateDatabaseWithMatchInformation(match):
                 "id": str(match['id']),
                 "away_name": match['away_name'],
                 "competition_name": match['competition_name'],
-                "country": match['location'],
                 "home_name": match['home_name'],
                 "location": match['location'],
                 "score": match['score'],
                 "time": match['time'],
-                "added": match['added']
+                "added": match['added'],
+                "country": match['country']['name'],
             }
     except:
-        print ("fail")
-        return "fail"
+        print("country is none, fixed it.")
+        parameters = \
+            {
+                "id": str(match['id']),
+                "away_name": match['away_name'],
+                "competition_name": match['competition_name'],
+                "home_name": match['home_name'],
+                "location": match['location'],
+                "score": match['score'],
+                "time": match['time'],
+                "added": match['added'],
+                "country": "N/A"
+            }
     try:
         response = requests.get(baseUrlBackend + "repeater", json=parameters)
-    except:
-        print("fail")
-        return "fail"
+    except Exception as e:
+        print(e)
+        print("faill")
+        return "faill"
     print("test")
     return response
     # print("\n\n\n\n\n\n" + parameters + "\n\n\n\n\n\n")
-   # response = requests.get(baseUrlBackend + "repeater", json=parameters) -
 
-    #return response -
+
+# response = requests.get(baseUrlBackend + "repeater", json=parameters) -
+
+# return response -
 
 # def ClearOldMatchesFromDailyMatchesAndMoveToHistory():
 #     return
 
+def CheckForFinishedMatches():
+    baseUrlBackend = "http://127.0.0.1:5000/"
+
+    try:
+        response = requests.get(baseUrlBackend + "checkMatches")
+    except Exception as e:
+        print(e)
+
+    if response.text == "not ok":
+        print("woopsie poopsie")
+
+    else:
+        print("OKAYYYYY")
+
+    return
+
+
 def GetDataFromFootballAPIAndUpdateDatabase():
     jsonData = GetDataFromFootballAPI()
-
 
     matches = jsonData['data']['match']
     for match in matches:
         UpdateDatabaseWithMatchInformation(match)
 
+
 moveToHistory = True
+prevTime = 1
 
 while True:
-    currentTime = time.localtime(time.time())
+    currentTime = time.time()
 
-    if currentTime.tm_hour == 1 and moveToHistory == True:
-        # ClearOldMatchesFromDailyMatchesAndMoveToHistory()
-        moveToHistory = False
+    if currentTime - prevTime > 60:
+        GetDataFromFootballAPIAndUpdateDatabase()
+        CheckForFinishedMatches()
 
-    if currentTime.tm_hour == 2:
-        moveToHistory = True
-
-    GetDataFromFootballAPIAndUpdateDatabase()
-    print("still running")
-    sleep(60)
+        prevTime = currentTime
