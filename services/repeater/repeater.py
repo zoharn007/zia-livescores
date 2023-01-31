66d90ac4761fa64bb1cc6d3d6c412b653726a8b2
ariel <ariel1434@gmail.com>
2023-01-31 14:45:46 +0200

    repeater.py backend-app-dev.dev

--- services/repeater/repeater.py
+++ services/repeater/repeater.py
@@ -21,7 +21,7 @@ def GetDataFromFootballAPI():
 
 
 def UpdateDatabaseWithMatchInformation(match):
-    baseUrlBackend = "http://127.0.0.1:5000/"
+    baseUrlBackend = "http://backend-app-dev.dev:5000/"
 
     try:
         parameters = \
@@ -67,7 +67,7 @@ def UpdateDatabaseWithMatchInformation(match):
 #     return
 
 def CheckForFinishedMatches():
-    baseUrlBackend = "http://127.0.0.1:5000/"
+    baseUrlBackend = "http://backend-app-dev.dev:5000/"
 
     try:
         response = requests.get(baseUrlBackend + "checkMatches")
