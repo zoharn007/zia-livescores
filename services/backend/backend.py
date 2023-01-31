a186f439f33df2a16a77f1590e792fa4a3904c40
ariel <ariel1434@gmail.com>
2023-01-31 21:07:02 +0200

    testing itay backend.py

--- services/backend/backend.py
+++ services/backend/backend.py
@@ -597,4 +597,4 @@ def getSubscriptionListByMatchID(matchID):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='backend-app-dev.test', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
